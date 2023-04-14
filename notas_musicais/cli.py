30608a59c4026f3b6cc73f136cf5b2031ba6c1dc
RWallan <3am.richardwallan@gmail.com>
2023-04-14 18:10:22 -0300

    ✨Implementação do subcomando de campos-harmonicos

--- notas_musicais/cli.py
+++ notas_musicais/cli.py
@@ -3,6 +3,7 @@ from rich.table import Table
 from typer import Argument, Typer
 
 from notas_musicais.acordes import acorde as _acorde
+from notas_musicais.campo_harmonico import campo_harmonico as _campo_harmonico
 from notas_musicais.escalas import escala as _escala
 
 console = Console()
@@ -34,5 +35,21 @@ def acorde(cifra: str = Argument('C', help='Cifra de um acorde.')):
     for grau in graus:
         table.add_column(grau)
     table.add_row(*notas)
+
+    console.print(table)
+
+
+@app.command()
+def campo_harmonico(
+    nota: str = Argument('C', help='Tônica do campo harmônico.'),
+    tonalidade: str = Argument('maior', help='Tonalidade do campo harmônico.'),
+):
+    table = Table()
+
+    acordes, graus = _campo_harmonico(nota, tonalidade).values()
+
+    for grau in graus:
+        table.add_column(grau)
+    table.add_row(*acordes)
 
     console.print(table)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -45,3 +45,17 @@ def test_acorde_cli_deve_conter_todos_os_graus(grau):
     resultado = runner.invoke(app, ['acorde', 'F'])
 
     assert grau in resultado.stdout
+
+
+@mark.parametrize('acordes', ['C', 'Dm', 'Em', 'F', 'G', 'Am', 'B°'])
+def test_campo_harmonico_cli_deve_retornar_os_acordes_na_resposta(acordes):
+    resultado = runner.invoke(app, ['campo-harmonico', 'C'])
+
+    assert acordes in resultado.stdout
+
+
+@mark.parametrize('grau', ['I', 'ii', 'iii', 'IV', 'V', 'vi', 'vii°'])
+def test_campo_harmonico_cli_deve_conter_todos_os_graus(grau):
+    resultado = runner.invoke(app, ['campo-harmonico', 'C'])
+
+    assert grau in resultado.stdout
