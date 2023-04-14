1048fc8ddeac7ce1a3de514d7903649ec0826c0a
RWallan <3am.richardwallan@gmail.com>
2023-04-14 15:46:55 -0300

    ✨Implementação do comando de acordes no comando de CLI

--- notas_musicais/cli.py
+++ notas_musicais/cli.py
@@ -2,20 +2,34 @@ from rich.console import Console
 from rich.table import Table
 from typer import Argument, Typer
 
-from notas_musicais.escalas import escala
+from notas_musicais.acordes import acorde as _acorde
+from notas_musicais.escalas import escala as _escala
 
 console = Console()
 app = Typer()
 
 
 @app.command()
-def escalas(
+def escala(
     nota: str = Argument('C', help='Tônica da escala.'),
     tonalidade: str = Argument('maior', help='Tonalidade da escala.'),
 ):
     table = Table()
 
-    notas, graus = escala(nota, tonalidade).values()
+    notas, graus = _escala(nota, tonalidade).values()
+
+    for grau in graus:
+        table.add_column(grau)
+    table.add_row(*notas)
+
+    console.print(table)
+
+
+@app.command()
+def acorde(cifra: str = Argument('C', help='Cifra de um acorde.')):
+    table = Table()
+
+    notas, graus = _acorde(cifra).values()
 
     for grau in graus:
         table.add_column(grau)
